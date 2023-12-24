fccf527fefb0af8d593fe629f6a544ef7808a7f4
Sarvesh207 <sarveshgaynar@gmail.com>
2023-12-24 13:13:15 +0530

    done with the accordian

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import { BG_URL } from '../utils/constant'
 import { HeroSection, Watch, Download, CreateProfile } from '../components/index'
 import Enjoy from '../components/Enjoy'
+import Faq from '../components/Faq'
+import  {faqData}  from '../Data/faqData'
 
 const LandingPage = () => {
+
+  console.log(faqData)
   return (
    <div >
    
@@ -12,6 +16,19 @@ const LandingPage = () => {
     <Download/>
     <Watch/>
     <CreateProfile/>
+    <div>
+      <h2 className='text-white text-center lg:text-5xl md:text-5xl text-3xl font-bold py-10'>Frequently Asked <br className='lg:hidden md:hidden'/> Questions</h2>
+      <div>
+        {
+          faqData.map((item, index) => {
+            const { title, desc} = item
+           return  <Faq key ={index} title={title} desc={desc}/>
+          })
+        }
+       
+      </div>
+      
+    </div>
    </div>
   )
 }
